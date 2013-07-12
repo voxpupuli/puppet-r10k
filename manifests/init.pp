badaf08fc97bae81851412fe75f2f8d7835db9a7
Gary Larizza <gary@puppetlabs.com>
2013-07-12 13:22:15 -0700

    Add support for changing source name in r10k.yaml
    Previously, this module was hardcoded to use the source 'jiminy'
    in r10k.yaml.  This commit adds the ability to pass a different source
    name via the $source_name parameter.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,11 @@
 # This class configures r10k
 class r10k (
-  $remote     = $r10k::params::remote,
-  $purgedirs  = $r10k::params::r10k_purgedirs,
-  $basedir    = $r10k::params::r10k_basedir,
-  $cachedir   = $r10k::params::r10k_cache_dir,
-  $configfile = $r10k::params::r10k_config_file,
+  $remote      = $r10k::params::remote,
+  $source_name = $r10k::params::source_name,
+  $purgedirs   = $r10k::params::r10k_purgedirs,
+  $basedir     = $r10k::params::r10k_basedir,
+  $cachedir    = $r10k::params::r10k_cache_dir,
+  $configfile  = $r10k::params::r10k_config_file,
 ) inherits r10k::params {
   if $::is_pe == 'true' {
     include r10k::pe
