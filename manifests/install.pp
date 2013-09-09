5ef7009ec83ca64d599c2f929173e26647c3f2d2
Reid Vandewiele <reid@puppetlabs.com>
2013-09-09 13:32:52 -0700

    Add back 0.0.9 support (make, gcc)
    There are currently bugs in r10k 1.x which make using 0.x desireable
    in certain circumstances. However, 0.x requires make and gcc.
    Conditionally include those classes if necessary due to 0.x r10k version
    usage.
    
    When 1.x is just as good or better than 0.x, we can stop supporting
    0.x and remove these bits.
    
    Conflicts:
    	manifests/install.pp

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,14 +3,22 @@ class r10k::install (
   $version,
   $pe_ruby,
 ) {
-  require gcc
-  require make
+
+  # There are currently bugs in r10k 1.x which make using 0.x desireable in
+  # certain circumstances. However, 0.x requires make and gcc. Conditionally
+  # include those classes if necessary due to 0.x r10k version usage. When
+  # 1.x is just as good or better than 0.x, we can stop supporting 0.x and
+  # remove this block.
+  if versioncmp('1.0.0', $version) > 0 {
+    require gcc
+    require make
+  }
 
   if $pe_ruby {
     $provider = 'pe_gem'
   } else {
     $provider = 'gem'
-    include 'r10k::install::ruby'
+    class { 'r10k::install::ruby': version => $version; }
   }
 
   package { 'r10k':
--- manifests/install/ruby.pp
+++ manifests/install/ruby.pp
@@ -1,8 +1,7 @@
 # Install the r10k gem using system ruby
-class r10k::install::ruby {
-
-  # Breaking up my chaining a little here
-  Class['::ruby'] -> Class['ruby::dev'] -> Package['gcc']
+class r10k::install::ruby (
+  $version,
+) {
 
   # rubygems_update => false
   # https://projects.puppetlabs.com/issues/19741
@@ -13,8 +12,17 @@ class r10k::install::ruby {
     tag => 'amineeded',
   }
 
-  # I am not sure this is required as I assumed the
-  # ruby::dev class would have taken care of it
-  Package['gcc'] -> Package['make'] -> Package['r10k']
+  # Explicit dependency chaining to make sure the system is ready to compile
+  # native extentions for dependent rubygems by the time r10k installation
+  # begins
+  if versioncmp('1.0.0', $version) > 0 {
+    # I am not sure all of this is required as I assumed the
+    # ruby::dev class would have taken care of some of it
+    Class['::ruby']    ->
+    Class['ruby::dev'] ->
+    Package['gcc']     ->
+    Package['make']    ->
+    Package['r10k']
+  }
 
 }
