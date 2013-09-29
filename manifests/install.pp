8ddc969e4bb06be061842846e1059f83706b620a
Adam Bothun-Hill <abothun-hill@sourceallies.com>
2013-09-29 17:47:18 -0500

    Install using bundle option added for bleeding edge features not in gem (like prefix at time of writing this)

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,8 +2,8 @@
 class r10k::install (
   $version,
   $pe_ruby,
+  $use_bundle,
 ) {
-
   # There are currently bugs in r10k 1.x which make using 0.x desireable in
   # certain circumstances. However, 0.x requires make and gcc. Conditionally
   # include those classes if necessary due to 0.x r10k version usage. When
@@ -21,9 +21,33 @@ class r10k::install (
     class { 'r10k::install::ruby': version => $version; }
   }
 
-  package { 'r10k':
-    ensure   => $version,
-    provider => $provider,
+  if ! $r10k::install::use_bundle {
+    package { 'r10k':
+      ensure   => $version,
+      provider => $provider,
+    }
+  } else {
+    # The bundle install has prefix support as of writing this, I want bleeding edge.
+    class { 'git': }
+    package { "${module_name}-bundle":
+      ensure   => installed,
+      name     => 'bundle',
+      provider => gem,
+    }
+    vcsrepo { "${module_name}-r10k-github":
+      ensure   => latest,
+      provider => git,
+      path     => '/tmp/r10k',
+      source   => 'https://github.com/adrienthebo/r10k.git',
+      revision => 'master',
+      require  => Class['git'],
+    }
+    exec { "${module_name}-install-via-bundle":
+      command => 'bundle && bundle install --path /opt/ --binstubs /usr/local/bin/',
+      cwd     => '/tmp/r10k',
+      require => [ Package["${module_name}-bundle"] , Vcsrepo["${module_name}-r10k-github"] ],
+      unless  => 'bundle list | grep -q " r10k "',
+      path    => '/usr/bin:/usr/local/bin:/usr/sbin:/usr/local/sbin:/sbin:/bin',
+    }
   }
-
 }
