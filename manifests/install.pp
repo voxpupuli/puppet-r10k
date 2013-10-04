988b21d10d69e4c75f407aed557763f2aa17977a
Theo Chatzimichos <tampakrap@gentoo.org>
2013-10-04 17:46:54 +0200

    Move bundle installation to r10k::install::bundle

--- manifests/install.pp
+++ manifests/install.pp
@@ -14,30 +14,7 @@ class r10k::install (
   }
 
   case $provider {
-    'bundle': {
-      # The bundle install has prefix support as of writing this, I want bleeding edge.
-      class { 'git': }
-      package { "${module_name}-bundle":
-        ensure   => installed,
-        name     => 'bundle',
-        provider => gem,
-      }
-      vcsrepo { "${module_name}-r10k-github":
-        ensure   => latest,
-        provider => git,
-        path     => '/tmp/r10k',
-        source   => 'https://github.com/adrienthebo/r10k.git',
-        revision => 'master',
-        require  => Class['git'],
-      }
-      exec { "${module_name}-install-via-bundle":
-        command => 'bundle && bundle install --path /opt/ --binstubs /usr/local/bin/',
-        cwd     => '/tmp/r10k',
-        require => [ Package["${module_name}-bundle"] , Vcsrepo["${module_name}-r10k-github"] ],
-        unless  => 'bundle list | grep -q " r10k "',
-        path    => '/usr/bin:/usr/local/bin:/usr/sbin:/usr/local/sbin:/sbin:/bin',
-      }
-    }
+    'bundle': { include r10k::install::bundle }
     'pe_gem', 'gem': {
       if $provider == 'gem' {
         class { 'r10k::install::gem': version => $version; }
