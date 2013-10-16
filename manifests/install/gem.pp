fc27abda900f7dd10b1ae77aa3eaf9979f9b274f
Zack Smith <acidprime@wallcity.org>
2013-10-16 21:57:13 +0000

    Add conditional logic to ruby decl to close #17
    Prior to this commit we did not include the ruby class, causing issues
    with sites that use the ruby class outside of r10k. This class attempts
    to help that by only doing it as required. From here we could also look
    at using if defined with params function from stdlib, but thats parse
    order dependant.

--- manifests/install/gem.pp
+++ manifests/install/gem.pp
@@ -3,14 +3,18 @@ class r10k::install::gem (
   $version,
 ) {
 
-  # rubygems_update => false
-  # https://projects.puppetlabs.com/issues/19741
-  class { '::ruby':
-    rubygems_update => false,
+  # Ideally we would be singleton here but due to the bug we need the param.
+  # If we are newer then the failure state, we do the right thing with include
+  if versioncmp($::puppetversion,'3.2.2') > 0 {
+    # https://projects.puppetlabs.com/issues/19663
+    class { '::ruby':
+      rubygems_update => false,
+    }
+  } else {
+    include ruby
   }
-  class { 'ruby::dev':
-    tag => 'amineeded',
-  }
+
+  include 'ruby::dev'
 
   # Explicit dependency chaining to make sure the system is ready to compile
   # native extentions for dependent rubygems by the time r10k installation
