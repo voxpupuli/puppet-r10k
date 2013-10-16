4510591a8cc5f04e9cbff3183f263a2e9f733880
Zack Smith <acidprime@wallcity.org>
2013-10-16 04:43:57 +0000

    Skeleton Spec Tests
    Prior to this commit we did not have a skeleton for the spec tests
    with all the fixtures needed. Now we do

--- Rakefile
+++ Rakefile
@@ -1,7 +1,4 @@
 require 'rake'
 
 require 'rspec/core/rake_task'
-
-RSpec::Core::RakeTask.new(:spec) do |t|
-  t.pattern = 'spec/*/*_spec.rb'
-end
+require 'puppetlabs_spec_helper/rake_tasks'
---
+++ spec/classes/r10k_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+describe 'r10k' , :type => 'class' do
+  context "on a RedHat 5 OS" do
+    let :params do
+      { :remote => 'git@github.com:someuser/puppet.git' }
+    end
+    let :facts do
+      {
+        :osfamily               => 'RedHat',
+        :operatingsystemrelease => '5',
+      }
+    end
+    it { should include_class('r10k::install') }
+    it { should include_class('r10k::config') }
+  end
+end
+
