b0931bba1c339883c6c39a6aca4af21007a65186
Zack Smith <acidprime@wallcity.org>
2013-10-17 03:14:22 +0000

    Add spec test for mcollective class
    Prior to this commit we did not have a test for the mcollective test
    This commit adds new tests as an intial commit

---
+++ spec/classes/mcollective_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+describe 'r10k::mcollective' , :type => 'class' do
+  context "Puppet Enterprise on a RedHat 5 OS installing mcollective agent & application" do
+    let :facts do
+      {
+        :osfamily               => 'RedHat',
+        :operatingsystemrelease => '5',
+        :operatingsystem        => 'Centos',
+        :is_pe                  => 'true'
+      }
+    end
+    it { should include_class("r10k::params") }
+    it { should contain_file("/opt/puppet/libexec/mcollective/mcollective/application/r10k.rb").with(
+        'ensure'   => 'present',
+        'owner'    => 'root',
+        'group'    => 'root',
+        'mode'     => '0644',
+        'notify'   => 'Service[pe-mcollective]'
+      )
+    }
+  end
+  context "Puppet FOSS on a RedHat 5 OS installing mcollective agent & application" do
+    let :facts do
+      {
+        :osfamily               => 'RedHat',
+        :operatingsystemrelease => '5',
+        :operatingsystem        => 'Centos',
+        :is_pe                  => ''
+      }
+    end
+    it { should include_class("r10k::params") }
+    it { should contain_file("/usr/libexec/mcollective/mcollective/application/r10k.rb").with(
+        'ensure'   => 'present',
+        'owner'    => 'root',
+        'group'    => 'root',
+        'mode'     => '0644',
+        'notify'   => 'Service[mcollective]'
+      )
+    }
+  end
+  context "Puppet FOSS on a Debian 6 OS installing mcollective agent & application" do
+    let :facts do
+      {
+        :operatingsystemrelease => '6',
+        :operatingsystem        => 'Ubuntu',
+        :osfamily               => 'Debian',
+        :is_pe                  => ''
+      }
+    end
+    it { should include_class("r10k::params") }
+    it { should_not contain_file("/usr/libexec/mcollective/mcollective/application/r10k.rb") }
+    it { should contain_file("/usr/share/mcollective/plugins/mcollective/application/r10k.rb").with(
+        'ensure'   => 'present',
+        'owner'    => 'root',
+        'group'    => 'root',
+        'mode'     => '0644',
+        'notify'   => 'Service[mcollective]'
+      )
+    }
+  end
+end
