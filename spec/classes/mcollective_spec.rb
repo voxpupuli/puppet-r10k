5199e2923b2312636d5752dbf945754eaa8bcf8f
Zack Smith <acidprime@wallcity.org>
2013-10-17 18:30:12 +0000

    Update mcollective spec tests
    Prior to this commit we did not fully check all file resources in the
    mcollective class. This commit adds new file resource checks

--- spec/classes/mcollective_spec.rb
+++ spec/classes/mcollective_spec.rb
@@ -18,6 +18,24 @@ describe 'r10k::mcollective' , :type => 'class' do
         'notify'   => 'Service[pe-mcollective]'
       )
     }
+    it { should contain_file("/opt/puppet/libexec/mcollective/mcollective/agent/r10k.ddl").with(
+        'ensure'   => 'present',
+        'owner'    => 'root',
+        'group'    => 'root',
+        'mode'     => '0644',
+        'notify'   => 'Service[pe-mcollective]'
+      )
+    }
+
+    it { should contain_file("/opt/puppet/libexec/mcollective/mcollective/agent/r10k.rb").with(
+        'ensure'   => 'present',
+        'owner'    => 'root',
+        'group'    => 'root',
+        'mode'     => '0644',
+        'notify'   => 'Service[pe-mcollective]'
+      )
+    }
+
   end
   context "Puppet FOSS on a RedHat 5 OS installing mcollective agent & application" do
     let :facts do
@@ -37,6 +55,23 @@ describe 'r10k::mcollective' , :type => 'class' do
         'notify'   => 'Service[mcollective]'
       )
     }
+    it { should contain_file("/usr/libexec/mcollective/mcollective/agent/r10k.ddl").with(
+        'ensure'   => 'present',
+        'owner'    => 'root',
+        'group'    => 'root',
+        'mode'     => '0644',
+        'notify'   => 'Service[mcollective]'
+      )
+    }
+                                                                                                
+    it { should contain_file("/usr/libexec/mcollective/mcollective/agent/r10k.rb").with(
+        'ensure'   => 'present',
+        'owner'    => 'root',
+        'group'    => 'root',
+        'mode'     => '0644',
+        'notify'   => 'Service[mcollective]'
+      )
+    }
   end
   context "Puppet FOSS on a Debian 6 OS installing mcollective agent & application" do
     let :facts do
@@ -49,7 +84,26 @@ describe 'r10k::mcollective' , :type => 'class' do
     end
     it { should include_class("r10k::params") }
     it { should_not contain_file("/usr/libexec/mcollective/mcollective/application/r10k.rb") }
+
     it { should contain_file("/usr/share/mcollective/plugins/mcollective/application/r10k.rb").with(
+        'ensure'   => 'present',
+        'owner'    => 'root',
+        'group'    => 'root',
+        'mode'     => '0644',
+        'notify'   => 'Service[mcollective]'
+      )
+    }
+
+    it { should contain_file("/usr/share/mcollective/plugins/mcollective/agent/r10k.ddl").with(
+        'ensure'   => 'present',
+        'owner'    => 'root',
+        'group'    => 'root',
+        'mode'     => '0644',
+        'notify'   => 'Service[mcollective]'
+      )
+    }
+
+    it { should contain_file("/usr/share/mcollective/plugins/mcollective/agent/r10k.rb").with(
         'ensure'   => 'present',
         'owner'    => 'root',
         'group'    => 'root',
