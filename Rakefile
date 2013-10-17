738c4a1f39058d6f182df228f36cc95dc440e66f
Zack Smith <acidprime@wallcity.org>
2013-10-17 00:21:45 +0000

    Add install_spec tests
    Prior to this commit we did not have any spec tests on the `r10k::install`
    This commit is the intial support for the different install types.
    
    I have commented out the gentoo stuff for the time being as I was not
    able to get it working. I also need to discuss with @tampakrap what an
    example declaration of the class looks like on gentoo ( i.e. what keywords)

--- Rakefile
+++ Rakefile
@@ -1,4 +1,1 @@
-require 'rake'
-
-require 'rspec/core/rake_task'
 require 'puppetlabs_spec_helper/rake_tasks'
---
+++ spec/classes/install_spec.rb
@@ -0,0 +1,126 @@
+require 'spec_helper'
+describe 'r10k::install' , :type => 'class' do
+  context "on a RedHat 5 OS installing 1.1.0 with gem provider" do
+    let :params do
+      {
+        :version  => '1.1.0',
+        :provider => 'gem',
+        :keywords => '',
+      }
+    end
+    let :facts do
+      {
+        :osfamily               => 'RedHat',
+        :operatingsystemrelease => '5',
+      }
+    end
+    it { should include_class("git") }
+
+    # New versions of this gem do not require these packages
+    it { should_not include_class("make")}
+    it { should_not contain_package("gcc")}
+
+    it { should contain_class("r10k::install::gem").with(
+       'version' => '1.1.0'
+      )
+    }
+    it { should contain_package("r10k").with(
+      'ensure'   => '1.1.0',
+      'provider' => 'gem'
+      )
+    }
+  end
+  context "on a RedHat 5 OS installing 0.0.9 with gem provider" do
+    let :params do
+      {
+        :version  => '0.0.9',
+        :provider => 'gem',
+        :keywords => '',
+      }
+    end
+    let :facts do
+      {
+        :osfamily               => 'RedHat',
+        :operatingsystem        => 'Centos',
+        :operatingsystemrelease => '5',
+      }
+    end
+    it { should include_class("git") }
+
+    # On older versions of r10k we need gcc & make
+    it { should include_class("gcc") }
+    it { should include_class("make") }
+
+    it { should contain_class("r10k::install::gem").with(
+       'version' => '0.0.9'
+      )
+    }
+    it { should contain_package("r10k").with(
+      'ensure'   => '0.0.9',
+      'provider' => 'gem'
+      )
+    }
+  end
+  context "on a RedHat 5 OS installing 1.1.0 with pe_gem provider" do
+    let :params do
+      {
+        :version  => '1.1.0',
+        :provider => 'pe_gem',
+        :keywords => '',
+      }
+    end
+    let :facts do
+      {
+        :osfamily               => 'RedHat',
+        :operatingsystemrelease => '5',
+      }
+    end
+    it { should include_class("git") }
+    it { should include_class("r10k::install::pe_gem") }
+    it { should contain_package("r10k").with(
+      'ensure'   => '1.1.0',
+      'provider' => 'pe_gem'
+      )
+    }
+  end
+  context "on a RedHat 5 OS installing 1.1.0 with bundle provider" do
+    let :params do
+      {
+        :version  => '1.1.0',
+        :provider => 'bundle',
+        :keywords => '',
+      }
+    end
+    let :facts do
+      {
+        :osfamily               => 'RedHat',
+        :operatingsystemrelease => '5',
+      }
+    end
+    it { should include_class("git") }
+    it { should include_class("r10k::install::bundle") }
+    it { should_not contain_package("r10k")}
+  end
+  #context "on a Gentoo OS installing 1.1.0 with portage provider" do
+  #  let :params do
+  #    {
+  #      :version  => '1.1.0',
+  #      :provider => 'portage',
+  #      :keywords => 'foo',
+  #    }
+  #  end
+  #  let :facts do
+  #    {
+  #      :osfamily               => 'Gentoo',
+  #      :operatingsystemrelease => '2.1',
+  #    }
+  #  end
+  #  it { should include_class("git") }
+  #  it { should contain_class("r10k::install::portage").with(
+  #      :keywords => 'foo',
+  #      :version  => '1.1.0'
+  #    )
+  #  }
+  #  it { should_not contain_package("r10k")}
+  #end
+end
