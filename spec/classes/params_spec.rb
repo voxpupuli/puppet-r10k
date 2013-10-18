1d1dceb3a9ed54ae9f696855a8668beea086994d
Zack Smith <acidprime@wallcity.org>
2013-10-18 00:46:37 +0000

    Add spec tests for params and prerun_command
    Prior to this commit we did not have spec tests for the params and
    prerun_command classes. This commit adds spec tests for these classes

---
+++ spec/classes/params_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+describe 'r10k::params' , :type => 'class' do
+  context "Puppet Enterprise on a RedHat 5 OS" do
+    let :facts do
+      {
+        :osfamily               => 'RedHat',
+        :operatingsystemrelease => '5',
+        :operatingsystem        => 'Centos',
+        :is_pe                  => 'true'
+      }
+    end
+    it { should contain_r10k__params }
+
+    it "Should not contain any resources" do
+      subject.resources.size.should == 4
+    end
+  end
+end
---
+++ spec/classes/prerun_command_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+describe 'r10k::prerun_command' , :type => 'class' do
+  context "Puppet Enterprise on a RedHat 5 OS installing adding custom prerun_command" do
+    let :params do
+      {
+        :command               => 'r10k synchronize',
+      }
+    end
+    let :facts do
+      {
+        :osfamily               => 'RedHat',
+        :operatingsystemrelease => '5',
+        :operatingsystem        => 'Centos',
+        :is_pe                  => 'true'
+      }
+    end
+    it { should include_class("r10k::params") }
+    it { should contain_ini_setting("r10k_prerun_command").with(
+        'ensure'  => 'present',
+        'section' => 'agent',
+        'setting' => 'prerun_command',
+        'value'   => 'r10k synchronize',
+        'path'    => '/etc/puppetlabs/puppet/puppet.conf'
+      )
+    }
+  end
+end
