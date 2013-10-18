d206912bef7be8080cf846b52a223cbbf948555e
Zack Smith <acidprime@wallcity.org>
2013-10-18 04:44:27 +0000

    Fix Tests for puppet 3.2.3+ and r10k 1.0.0+
    Prior to this commit our spec test were failing as the logic in the
    class was backwards in for the respective conditions

--- manifests/install/gem.pp
+++ manifests/install/gem.pp
@@ -5,7 +5,7 @@ class r10k::install::gem (
 
   # Ideally we would be singleton here but due to the bug we need the param.
   # If we are newer then the failure state, we do the right thing with include
-  if versioncmp($::puppetversion,'3.2.2') > 0 {
+  if versioncmp($::puppetversion,'3.2.2') < 0 {
     # https://projects.puppetlabs.com/issues/19663
     class { '::ruby':
       rubygems_update => false,
