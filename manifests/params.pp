61100cbb8fc24d0ec24d7824c96fb41306aa9486
Theo Chatzimichos <tampakrap@gentoo.org>
2013-11-26 16:19:08 +0100

    cosmetics

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,15 +7,15 @@ class r10k::params
   $install_options   = ''
 
   # r10k configuration
-  $r10k_config_file     = '/etc/r10k.yaml'
-  $r10k_cache_dir       = '/var/cache/r10k'
-  $r10k_basedir         = "${::settings::confdir}/environments"
-  $r10k_purgedirs       = $r10k_basedir
+  $r10k_config_file = '/etc/r10k.yaml'
+  $r10k_cache_dir   = '/var/cache/r10k'
+  $r10k_basedir     = "${::settings::confdir}/environments"
+  $r10k_purgedirs   = $r10k_basedir
 
   # Git configuration
-  $git_server          = $::settings::ca_server
-  $repo_path           = '/var/repos'
-  $remote              = "ssh://${git_server}${repo_path}/modules.git"
+  $git_server = $::settings::ca_server
+  $repo_path  = '/var/repos'
+  $remote     = "ssh://${git_server}${repo_path}/modules.git"
 
   # prerun_command in puppet.conf
   $prerun_command = 'r10k deploy environment -p'
@@ -25,32 +25,33 @@ class r10k::params
 
   if $::is_pe == 'true' {
     # Puppet Enterprise specific settings
-    $puppetconf_path     = '/etc/puppetlabs/puppet'
+    $puppetconf_path = '/etc/puppetlabs/puppet'
 
-    $pe_module_path      = '/opt/puppet/share/puppet/modules'
+    $pe_module_path  = '/opt/puppet/share/puppet/modules'
     # Mcollective configuration dynamic
-    $mc_service_name     = 'pe-mcollective'
-    $plugins_dir         = '/opt/puppet/libexec/mcollective/mcollective'
-    $modulepath          = "${r10k_basedir}/\$environment/modules:${pe_module_path}"
-    $provider            = 'pe_gem'
+    $mc_service_name = 'pe-mcollective'
+    $plugins_dir     = '/opt/puppet/libexec/mcollective/mcollective'
+    $modulepath      = "${r10k_basedir}/\$environment/modules:${pe_module_path}"
+    $provider        = 'pe_gem'
   } else {
     # Installation package manager provider
     $provider = 'gem'
 
     # Getting ready for FOSS support in this module
-    $puppetconf_path     = '/etc/puppet'
+    $puppetconf_path = '/etc/puppet'
 
     # Mcollective configuration dynamic
-    $mc_service_name     = 'mcollective'
+    $mc_service_name = 'mcollective'
+    $modulepath = "${r10k_basedir}/\$environment/modules"
+
     case $::osfamily {
-      'debian':  {
-        $plugins_dir  = '/usr/share/mcollective/plugins/mcollective'
+      'debian': {
+        $plugins_dir = '/usr/share/mcollective/plugins/mcollective'
       }
       default: {
-        $plugins_dir  = '/usr/libexec/mcollective/mcollective'
+        $plugins_dir = '/usr/libexec/mcollective/mcollective'
       }
     }
-    $modulepath          = "${r10k_basedir}/\$environment/modules"
   }
 
   # Mcollective configuration static
