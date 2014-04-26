a012dff0d1ce8a2bfe6b6bf56a82212dffc6b531
Garrett Honeycutt <code@garretthoneycutt.com>
2014-04-25 21:23:02 -0400

    Add parameter to optionally include prerun_command class
    This patch allows you to parameterize the inclusion of
    r10k::prerun_command and keep you from having to include a separate
    class. The code optionally does an include on the class in order to
    maintain backward compatibility. Recommend that in a future release the
    code from the prerun_command class is moved into the r10k class where it
    is currently being included.

---
+++ spec/classes/init_spec.rb
@@ -0,0 +1,70 @@
+require 'spec_helper'
+describe 'r10k' do
+  context 'with default values for params' do
+    let :facts do
+      {
+        :osfamily               => 'RedHat',
+        :operatingsystemrelease => '5',
+        :operatingsystem        => 'Centos',
+        :is_pe                  => 'true'
+      }
+    end
+
+    it { should compile.with_all_deps }
+    it { should_not contain_class('r10k::prerun_command') }
+  end
+
+  ['true',true].each do |value|
+    context "with param include_prerun_command set to #{value}" do
+      let(:params) { { :include_prerun_command => value } }
+      let :facts do
+        {
+          :osfamily               => 'RedHat',
+          :operatingsystemrelease => '5',
+          :operatingsystem        => 'Centos',
+          :is_pe                  => 'true'
+        }
+      end
+
+      it { should compile.with_all_deps }
+
+      it { should contain_class('r10k::prerun_command') }
+    end
+  end
+
+  ['false',false].each do |value|
+    context "with param include_prerun_command set to #{value}" do
+      let(:params) { { :include_prerun_command => value } }
+      let :facts do
+        {
+          :osfamily               => 'RedHat',
+          :operatingsystemrelease => '5',
+          :operatingsystem        => 'Centos',
+          :is_pe                  => 'true'
+        }
+      end
+
+      it { should compile.with_all_deps }
+
+      it { should_not contain_class('r10k::prerun_command') }
+    end
+  end
+
+  context 'with param include_prerun_command set to an invalid value' do
+    let(:params) { { :include_prerun_command => 'invalid' } }
+    let :facts do
+      {
+        :osfamily               => 'RedHat',
+        :operatingsystemrelease => '5',
+        :operatingsystem        => 'Centos',
+        :is_pe                  => 'true'
+      }
+    end
+
+    it 'should fail' do
+      expect {
+        should contain_class('r10k')
+      }.to raise_error(Puppet::Error)
+    end
+  end
+end
