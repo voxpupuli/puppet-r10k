c079adb866ceadfaad38869bac4853708d738933
Zack Smith <zack@puppetlabs.com>
2014-09-02 12:30:33 -0700

    Update version

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@
 class r10k::params
 {
   $package_name      = ''
-  $version           = '1.3.1'
+  $version           = '1.3.2'
   $manage_modulepath = false
   $install_options   = ''
   $sources           = undef
