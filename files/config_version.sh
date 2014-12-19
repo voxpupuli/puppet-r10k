2e2b0ba6d63355323a7c39a44f3eb34497bbdf8c
Zack Smith <zack@puppetlabs.com>
2014-12-19 11:06:04 -0800

    Add config_version.sh example
    Prior to this commit we had a pre-commit exmaple but not a
    config_version example. This commit adds an example github script that
    changes your prompt to something like this:
    
    ```shell
    Info: Applying configuration version '[Fix
    string](http://github.com/acidprime/puppet/tree/9e04bcea97bc5903b0328af3892ef95650b8fde9)'
    ```

---
+++ files/config_version.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Create a environment.conf in your control repo
+# and set the path to this script i.e.
+# config_version = scripts/config_version.sh $environment
+# https://docs.puppetlabs.com/puppet/latest/reference/config_file_environment.html#configversion
+# Return the latest commit with the GitHub URL for it
+# The repos are placed using r10k
+
+# Get the hash of the latest commit
+sha=$(git --git-dir \
+  /etc/puppetlabs/puppet/environments/$1/.git rev-parse HEAD)
+
+message=$(git --git-dir \
+  /etc/puppetlabs/puppet/environments/$1/.git log -1 --pretty='%s')
+
+# Get the address for remote origin
+remote=$(git --git-dir \
+  /etc/puppetlabs/puppet/environments/$1/.git config --get remote.origin.url)
+
+# Sanitize the git remote to be an http url.
+#url=$(echo "${remote}"|/bin/sed -e 's/git@//g; s/:/\//g; s/.git$//g')
+url=$(echo "${remote}"| awk -F'[@:]' '{ sub(/.git$/,"");print "http://"$2"/"$3}')
+
+# Return a full URL for it
+echo "[${message}](${url}/tree/${sha})"
