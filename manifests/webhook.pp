f69511d7bbd339e1a3542930743d235a0b1f15cd
Eli Young <eyoung@certifydatasystems.com>
2015-01-23 18:47:13 -0800

    Remove +x permissions from peadmin-cert.pem

--- manifests/webhook.pp
+++ manifests/webhook.pp
@@ -45,6 +45,7 @@ class r10k::webhook(
       ensure  => 'file',
       owner   => 'peadmin',
       group   => 'peadmin',
+      mode    => '0644',
       content => file('/etc/puppetlabs/puppet/ssl/certs/pe-internal-peadmin-mcollective-client.pem'),
   }
 
