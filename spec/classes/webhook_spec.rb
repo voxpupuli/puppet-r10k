9090a5f087634bebf7736e50589e5c7fff17fd39
Zack Smith <zack@puppetlabs.com>
2015-04-22 13:33:50 -0700

    Update specs
    Prior to this commit we were not running spec tests on the webhook
    causing Hilarity to Ensue this commit renames the file and cleans up
    the tests for the latest code

--- spec/classes/webhook_spec.rb
+++ spec/classes/webhook_spec.rb
@@ -10,8 +10,15 @@ describe 'r10k::webhook' , :type => 'class' do
         :pe_version             => '3.7.0'
       }
     end
-    it { should contain_package("rack").with(
+    it { should contain_package("sinatra").with(
         'ensure'   => 'installed',
+        'provider' => 'pe_gem',
+      )
+    }
+
+    it { should contain_package("sinatra").with(
+        'ensure'    => 'installed',
+        'provider'  => 'pe_gem',
       )
     }
     it { should contain_file("peadmin-cert.pem").with(
@@ -23,24 +30,24 @@ describe 'r10k::webhook' , :type => 'class' do
       )
     }
   end
-  context "Puppet FOSS on a RedHat 5 installing webhook" do
+  context "Puppet 2.7.0 FOSS on a RedHat 5 installing webhook" do
     let :facts do
       {
         :osfamily               => 'RedHat',
         :operatingsystemrelease => '5',
         :operatingsystem        => 'Centos',
+        :puppetversion          => '2.7.0',
       }
     end
-    it { should_not contain_package("rack").with(
-        'ensure'   => 'installed',
+
+    it { should contain_package("sinatra").with(
+        'ensure'    => 'installed',
+        'provider'  => 'gem',
       )
     }
-    it { should_not contain_file("peadmin-cert.pem").with(
-        'path'   => '/var/lib/peadmin/.mcollective.d/peadmin-cert.pem',
-        'ensure'  => 'file',
-        'owner'   => 'peadmin',
-        'group'   => 'peadmin',
-        'mode'    => '0644',
+
+    it { should_not contain_package("rack").with(
+        'ensure'   => 'installed',
       )
     }
   end
@@ -53,6 +60,13 @@ describe 'r10k::webhook' , :type => 'class' do
         :puppetversion          => '3.7.1',
       }
     end
+
+    it { should contain_package("sinatra").with(
+        'ensure'    => 'installed',
+        'provider'  => 'gem',
+      )
+    }
+
     it { should contain_package("webrick").with(
         'ensure'   => 'installed',
         'provider' => 'gem',
