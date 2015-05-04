c5026053503e2c44ce62330be60c141d04155e5a
Trevor Smith <tsmith3@entertainment.com>
2015-05-04 17:18:32 -0400

    change to only check the pe_version fact if the is_pe fact is 'true' or true, to account for stringified facts, as the getvar() approach still throws an error under future parser and puppet 4 on the foss version.

--- manifests/install.pp
+++ manifests/install.pp
@@ -64,7 +64,7 @@ class r10k::install (
 
       # Puppet Enterprise 3.8 and ships an embedded r10k so thats all thats supported
       # This conditional should not effect FOSS customers based on the fact 
-      unless getvar('::pe_version') and versioncmp($::pe_version, '3.8.0') >= 0 {
+      unless ($::is_pe == 'true' or $::is_pe == true) and versioncmp($::pe_version, '3.8.0') >= 0 {
         package { $real_package_name:
           ensure          => $version,
           provider        => $provider,
