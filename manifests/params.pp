70e45180f32d89456f612eb87d0ae2e5870c8278
Robin H. Johnson <robbat2@gentoo.org>
2015-05-13 18:37:37 -0700

    Add a Gentoo init script for webhook, based on the stock Gentoo init.d template.
    Signed-off-by: Robin H. Johnson <robbat2@gentoo.org>

--- manifests/params.pp
+++ manifests/params.pp
@@ -129,6 +129,9 @@ class r10k::params
   if $::osfamily == 'RedHat' and $::operatingsystemmajrelease == '7' {
     $webhook_service_file     = '/usr/lib/systemd/system/webhook.service'
     $webhook_service_template = 'webhook.service.erb'
+  } $::osfamily == 'Gentoo' {
+    $webhook_service_file     = '/etc/init.d/webhook'
+    $webhook_service_template = 'webhook.init.gentoo.erb'
   } else {
     $webhook_service_file     = '/etc/init.d/webhook'
     $webhook_service_template = 'webhook.init.erb'
