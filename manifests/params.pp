c687463f276302f5b8f2ec657896b4c39f01c8e3
Andrew Brader <abrader@gmail.com>
2015-07-21 22:14:32 +0000

    Second commit
    Per a discussion with Zack $::puppetversion must be relied on since it is the only piece of info consistent between versions of PE.  Altered code to utilizie this fact more thoroughly.
    Second commit
    
    Per a discussion with Zack $::puppetversion must be relied on since it is the only piece of info consistent between versions of PE.  Altered code to utilizie this fact more thoroughly.

--- manifests/params.pp
+++ manifests/params.pp
@@ -57,16 +57,38 @@ class r10k::params
     $functions_path     = '/etc/rc.d/init.d/functions'
     $start_pidfile_args = '--pidfile $pidfile'
   }
-
+  
   if $::is_pe == true or $::is_pe == 'true' {
-    $pe_server_version = $::pe_version
+    # < PE 4
+    $is_pe_server      = true
+  }elsif is_function_available('pe_compiling_server_version') {
+    # >= PE 4
+    $is_pe_server      = true
   }
-  elsif is_function_available('pe_compiling_server_version') {
-    $pe_server_version = pe_compiling_server_version()
+  else {
+    # FOSS
+    $is_pe_server      = false
   }
 
-  if $pe_server_version {
-    # Puppet Enterprise specific settings
+  if $is_pe_server and versioncmp($::puppetversion, '4.0.0') >= 0 {
+    # PE 4 or greater specific settings
+    $puppetconf_path = '/etc/puppetlabs/puppet'
+
+    $pe_module_path  = '/opt/puppetlabs/puppet/modules'
+    # Mcollective configuration dynamic
+    $mc_service_name = 'mcollective'
+    $plugins_dir     = '/opt/puppetlabs/mcollective/plugins'
+    $modulepath      = "${r10k_basedir}/\$environment/modules:${pe_module_path}"
+    $provider        = 'pe_gem'
+    $r10k_binary     = 'r10k'
+
+    # webhook
+    $webhook_user    = 'peadmin'
+    $webhook_pass    = 'peadmin'
+    $webhook_group   = 'peadmin'
+  }
+  elsif $is_pe_server and versioncmp($::puppetversion, '4.0.0') == -1 {
+    # PE 3.x.x specific settings
     $puppetconf_path = '/etc/puppetlabs/puppet'
 
     $pe_module_path  = '/opt/puppet/share/puppet/modules'
@@ -81,8 +103,9 @@ class r10k::params
     $webhook_user    = 'peadmin'
     $webhook_pass    = 'peadmin'
     $webhook_group   = 'peadmin'
-  } else {
-    # Getting ready for FOSS support in this module
+  }
+  else {
+    # FOSS specific settings
     $puppetconf_path = '/etc/puppet'
 
     # Mcollective configuration dynamic
