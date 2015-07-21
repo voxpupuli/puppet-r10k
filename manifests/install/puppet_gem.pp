275b76ad7409d96f495384914c6ef678ddcded5b
J. Zimmerman <john.zimm@gmail.com>
2015-07-21 16:05:28 -0700

    Add support for puppet_gem provider to account for included ruby/gem in Puppet FOSS 4.2.0 and higher.

---
+++ manifests/install/puppet_gem.pp
@@ -0,0 +1,13 @@
+# This class links the r10k binary for Puppet FOSS 4.2 and up
+class r10k::install::puppet_gem {
+
+  require git
+
+  unless versioncmp($::puppetversion, '4.2.0') >= 0 {
+    file { '/usr/bin/r10k':
+      ensure  => link,
+      target  => '/opt/puppetlabs/bin/r10k',
+      require => Package['r10k'],
+    }
+  }
+}
