dc3b542531d6417366d04646fb9a2c68440c0dd9
Andrew Brader <abrader@gmail.com>
2015-07-23 13:20:16 -0400

    Update install_spec.rb
    Typo, forgot a comma.

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -243,7 +243,7 @@ describe 'r10k::install' , :type => 'class' do
         :operatingsystemrelease => '5',
         :operatingsystem        => 'Centos',
         :is_pe                  => true,
-        :pe_version             => '3.7.0'
+        :pe_version             => '3.7.0',
         :puppetversion          => '3.7.0 (Puppet Enterprise 3.7.0)',
       }
     end
