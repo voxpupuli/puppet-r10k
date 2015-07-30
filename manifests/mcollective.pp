667d98041fedd532fd111bf780caf25bcbc71c95
Zack Smith <zack@puppetlabs.com>
2015-07-30 10:23:26 -0700

    Remove depricated param
    Prior to this commit the param had been renamed and showed a warning
    this commit removes the param based on semver

--- manifests/mcollective.pp
+++ manifests/mcollective.pp
@@ -7,7 +7,6 @@ class r10k::mcollective (
   $app_path          = $r10k::params::mc_application_path,
   $mc_service        = $r10k::params::mc_service_name,
   $http_proxy        = $r10k::params::mc_http_proxy,
-  $git_ssl_verify    = $r10k::params::mc_git_ssl_verify, # Deprecated
   $git_ssl_no_verify = $r10k::params::mc_git_ssl_no_verify,
 ) inherits r10k::params {
   File {
@@ -16,14 +15,7 @@ class r10k::mcollective (
     group  => '0',
     mode   => '0644',
   }
-
-  if $git_ssl_verify {
-    warning('$git_ssl_verify parameter is deprecated, please use $git_ssl_no_verify instead')
-    $agent_no_verify = $git_ssl_verify
-  } else {
-    $agent_no_verify = $git_ssl_no_verify
-  }
-
+  
   # Install the agent and its ddl file
   file { "${app_path}/${app_name}":
     source => "puppet:///modules/${module_name}/application/${agent_name}",
