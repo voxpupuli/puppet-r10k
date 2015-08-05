abe8df1c5467e017e454b4c1d40685c4b341a340
Ben Ford <ben.ford@puppetlabs.com>
2015-08-05 16:13:08 -0700

    remove extraneous indent

--- spec/classes/webhook/config_spec.rb
+++ spec/classes/webhook/config_spec.rb
@@ -21,26 +21,26 @@ describe 'r10k::webhook::config' , :type => 'class' do
     }
 
     content = """---
-  access_logfile: \"/var/log/webhook/access.log\"
-  bind_address: \"0.0.0.0\"
-  certname: \"peadmin\"
-  certpath: \"/var/lib/peadmin/.mcollective.d\"
-  client_cfg: \"/var/lib/peadmin/.mcollective\"
-  client_timeout: \"120\"
-  command_prefix: \"umask 0022;\"
-  discovery_timeout: \"10\"
-  enable_ssl: true
-  pass: \"peadmin\"
-  port: \"8088\"
-  prefix: false
-  prefix_command: \"/bin/echo example\"
-  private_key_path: \"/var/lib/peadmin/.mcollective.d/peadmin-private.pem\"
-  protected: true
-  public_key_path: \"/var/lib/peadmin/.mcollective.d/peadmin-cert.pem\"
-  r10k_deploy_arguments: \"-pv\"
-  use_mco_ruby: false
-  use_mcollective: true
-  user: \"peadmin\"
+access_logfile: \"/var/log/webhook/access.log\"
+bind_address: \"0.0.0.0\"
+certname: \"peadmin\"
+certpath: \"/var/lib/peadmin/.mcollective.d\"
+client_cfg: \"/var/lib/peadmin/.mcollective\"
+client_timeout: \"120\"
+command_prefix: \"umask 0022;\"
+discovery_timeout: \"10\"
+enable_ssl: true
+pass: \"peadmin\"
+port: \"8088\"
+prefix: false
+prefix_command: \"/bin/echo example\"
+private_key_path: \"/var/lib/peadmin/.mcollective.d/peadmin-private.pem\"
+protected: true
+public_key_path: \"/var/lib/peadmin/.mcollective.d/peadmin-cert.pem\"
+r10k_deploy_arguments: \"-pv\"
+use_mco_ruby: false
+use_mcollective: true
+user: \"peadmin\"
 """
     it { should contain_file('webhook.yaml').with_content(content) }
   end
@@ -64,23 +64,75 @@ describe 'r10k::webhook::config' , :type => 'class' do
     }
 
     content = """---
-  access_logfile: \"/var/log/webhook/access.log\"
-  bind_address: \"0.0.0.0\"
-  client_cfg: \"/var/lib/peadmin/.mcollective\"
-  client_timeout: \"120\"
-  command_prefix: \"umask 0022;\"
-  discovery_timeout: \"10\"
-  enable_ssl: true
-  pass: \"puppet\"
-  port: \"8088\"
-  prefix: false
-  prefix_command: \"/bin/echo example\"
-  protected: true
-  r10k_deploy_arguments: \"-pv\"
-  use_mco_ruby: false
-  use_mcollective: true
-  user: \"puppet\"
+access_logfile: \"/var/log/webhook/access.log\"
+bind_address: \"0.0.0.0\"
+client_cfg: \"/var/lib/peadmin/.mcollective\"
+client_timeout: \"120\"
+command_prefix: \"umask 0022;\"
+discovery_timeout: \"10\"
+enable_ssl: true
+pass: \"puppet\"
+port: \"8088\"
+prefix: false
+prefix_command: \"/bin/echo example\"
+protected: true
+r10k_deploy_arguments: \"-pv\"
+use_mco_ruby: false
+use_mcollective: true
+user: \"puppet\"
 """
     it { should contain_file('webhook.yaml').with_content(content) }
   end
+
+  context 'Puppet Enterprise 3.7.0 on a RedHat 5 installing webhook with events defined' do
+    let :facts do
+      {
+        :osfamily               => 'RedHat',
+        :operatingsystemrelease => '5',
+        :operatingsystem        => 'Centos',
+        :is_pe                  => 'true',
+        :pe_version             => '3.7.0'
+      }
+    end
+    let(:params) do
+      {
+        :repository_events => ['merge','release'],
+      }
+    end
+    it { should contain_file('webhook.yaml').with(
+        'path'   => '/etc/webhook.yaml',
+        'ensure' => 'file',
+        'owner'  => 'root',
+        'group'  => '0',
+        'mode'   => '0644',
+        'notify' => 'Service[webhook]'
+      )
+    }
+
+    content = """---
+access_logfile: \"/var/log/webhook/access.log\"
+bind_address: \"0.0.0.0\"
+certname: \"peadmin\"
+certpath: \"/var/lib/peadmin/.mcollective.d\"
+client_cfg: \"/var/lib/peadmin/.mcollective\"
+client_timeout: \"120\"
+command_prefix: \"umask 0022;\"
+discovery_timeout: \"10\"
+enable_ssl: true
+pass: \"peadmin\"
+port: \"8088\"
+prefix: false
+prefix_command: \"/bin/echo example\"
+private_key_path: \"/var/lib/peadmin/.mcollective.d/peadmin-private.pem\"
+protected: true
+public_key_path: \"/var/lib/peadmin/.mcollective.d/peadmin-cert.pem\"
+r10k_deploy_arguments: \"-pv\"
+repository_events: [\"merge\",\"release\"]
+use_mco_ruby: false
+use_mcollective: true
+user: \"peadmin\"
+"""
+    it { should contain_file('webhook.yaml').with_content(content) }
+  end
+
 end
