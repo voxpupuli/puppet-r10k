7ec09649585012a8d17d89bd00b7cd97de539597
Ben Ford <ben.ford@puppetlabs.com>
2015-08-05 16:14:58 -0700

    test events and array expansion

--- spec/classes/webhook/config_spec.rb
+++ spec/classes/webhook/config_spec.rb
@@ -127,7 +127,7 @@ private_key_path: \"/var/lib/peadmin/.mcollective.d/peadmin-private.pem\"
 protected: true
 public_key_path: \"/var/lib/peadmin/.mcollective.d/peadmin-cert.pem\"
 r10k_deploy_arguments: \"-pv\"
-repository_events: [\"merge\",\"release\"]
+repository_events: [\"merge\", \"release\"]
 use_mco_ruby: false
 use_mcollective: true
 user: \"peadmin\"
