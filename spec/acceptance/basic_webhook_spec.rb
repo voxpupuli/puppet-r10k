2e7e536151d70e4cb540e41f495e1f1dd205af9d
Zack Smith <zack@puppetlabs.com>
2015-08-06 15:15:00 -0700

    Add curl examples to beaker
    Prior to this commit we did not try the different types of posts
    this adds the cut and paste examples from the different contribubutions
    from other GMS that we support

--- spec/acceptance/basic_webhook_spec.rb
+++ spec/acceptance/basic_webhook_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper_acceptance'
+require 'json'
+describe 'System Ruby with No SSL, Not protected, No mcollective' do
 
-describe 'basic webhook' do
-
- context 'default parameters' do
+  context 'default parameters' do
     let(:pp) {"
       class { 'r10k':
         remote => 'git@github.com:someuser/puppet.git',
@@ -24,6 +24,39 @@ describe 'basic webhook' do
     it 'should be idempotent' do
       apply_manifest(pp, :catch_changes=>true)
     end
+    describe service('webhook') do
+      it { should be_enabled }
+      it { should be_running }
+    end
+    it 'should support style Github payloads via module end point' do
+      shell('/usr/bin/curl -d \'{ "repository": { "name": "puppetlabs-stdlib" } }\' -H "Accept: application/json" "http://localhost:8088/module" -k -q') do |r|
+        expect(r.stdout).to match(/^.*success.*$/)
+        expect(r.exit_code).to eq(0)
+      end
+    end
+    it 'should support style Bitbucket payloads via module end point' do
+      shell('/usr/bin/curl -X POST -d \'{ "repository": { "full_name": "puppetlabs/puppetlabs-stdlib", "name": "PuppetLabs : StdLib" } }\' "http://localhost:8088/module" -k -q') do |r|
+        expect(r.stdout).to match(/^.*success.*$/)
+        expect(r.exit_code).to eq(0)
+      end
+    end
+    it 'should support style Github payloads via payload end point' do
+      shell('/usr/bin/curl -d \'{ "ref": "refs/heads/production" }\' -H "Accept: application/json" "http://localhost:8088/payload" -k -q') do |r|
+        expect(r.stdout).to match(/^.*success.*$/)
+        expect(r.exit_code).to eq(0)
+      end
+    end
+    it 'should support style Gitorious payloads via payload end point' do
+      shell('/usr/bin/curl -X POST -d \'%7b%22ref%22%3a%22master%22%7d\' "http://localhost:8088/payload" -q') do |r|
+        expect(r.stdout).to match(/^.*success.*$/)
+        expect(r.exit_code).to eq(0)
+      end
+    end
+    it 'should support style BitBucket payloads via payload end point' do
+      shell('/usr/bin/curl -X POST -d \'{ "push": { "changes": [ { "new": { "name": "production" } } ] } }\' "http://localhost:8088/payload" -q') do |r|
+        expect(r.stdout).to match(/^.*success.*$/)
+        expect(r.exit_code).to eq(0)
+      end
+    end
   end
-
 end
