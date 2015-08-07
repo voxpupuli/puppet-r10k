f9e253df62a955937a93c50671a44bb262393eb8
Corey Osman <corey@logicminds.biz>
2015-08-07 12:46:08 -0700

    r10k fact throws errors to stdout
    * previously this was throwing the error output to stdout instead of to /dev/null upon every facter call
    * fixes conditional to check if command succeeding rather than if path exists, since the path will always be a string

--- lib/facter/r10k_path.rb
+++ lib/facter/r10k_path.rb
@@ -1,9 +1,9 @@
 Facter.add(:r10k_path) do
   confine :kernel => :linux
   setcode do
-    path = Facter::Util::Resolution.exec("which r10k")
-    if path
-      path.to_s
+    path = Facter::Util::Resolution.exec("which r10k 2> /dev/null")
+    if $?.success?
+      path
     else
       nil
     end
