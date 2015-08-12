080e281ae005dfc8494815e51a000b89715c1bb4
Zack Smith <zack@puppetlabs.com>
2015-08-11 17:15:23 -0700

    Remove unneeded logic
    Prior to this commit we checked the exit value of the
    command. This should be garded against by this
    https://github.com/puppetlabs/facter/blob/1.6.x/lib/facter/util/resolution.rb#L143-L147

--- lib/facter/r10k_path.rb
+++ lib/facter/r10k_path.rb
@@ -1,11 +1,6 @@
 Facter.add(:r10k_path) do
   confine :kernel => :linux
   setcode do
-    path = Facter::Util::Resolution.exec("which r10k 2> /dev/null")
-    if $?.success?
-      path
-    else
-      nil
-    end
+    Facter::Util::Resolution.exec("which r10k 2> /dev/null")
   end
 end
