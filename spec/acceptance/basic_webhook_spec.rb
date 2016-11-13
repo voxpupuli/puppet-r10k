80382c458e8597ad54093d0677739c949b343e2f
Rob Nelson <rnelson0@gmail.com>
2016-11-13 16:12:06 +0000

    Set Rspec/MultipleExpectations to 2

--- spec/acceptance/basic_webhook_spec.rb
+++ spec/acceptance/basic_webhook_spec.rb
@@ -37,26 +37,26 @@ describe 'System Ruby with No SSL, Not protected, No mcollective' do
     end
     it 'supports style Bitbucket payloads via module end point' do
       shell('/usr/bin/curl -X POST -d \'{ "repository": { "full_name": "puppetlabs/puppetlabs-stdlib", "name": "PuppetLabs : StdLib" } }\' "http://localhost:8088/module" -k -q') do |r|
-        expect(r.stdout).to match(%r{^.*success.*$}) # rubocop:disable Rspec/MultipleExpectations
-        expect(r.exit_code).to eq(0) # rubocop:disable Rspec/MultipleExpectations
+        expect(r.stdout).to match(%r{^.*success.*$})
+        expect(r.exit_code).to eq(0)
       end
     end
     it 'supports style Github payloads via payload end point' do
       shell('/usr/bin/curl -d \'{ "ref": "refs/heads/production" }\' -H "Accept: application/json" "http://localhost:8088/payload" -k -q') do |r|
-        expect(r.stdout).to match(%r{^.*success.*$}) # rubocop:disable Rspec/MultipleExpectations
-        expect(r.exit_code).to eq(0) # rubocop:disable Rspec/MultipleExpectations
+        expect(r.stdout).to match(%r{^.*success.*$})
+        expect(r.exit_code).to eq(0)
       end
     end
     it 'supports style Gitorious payloads via payload end point' do
       shell('/usr/bin/curl -X POST -d \'%7b%22ref%22%3a%22master%22%7d\' "http://localhost:8088/payload" -q') do |r|
-        expect(r.stdout).to match(%r{^.*success.*$}) # rubocop:disable Rspec/MultipleExpectations
-        expect(r.exit_code).to eq(0) # rubocop:disable Rspec/MultipleExpectations
+        expect(r.stdout).to match(%r{^.*success.*$})
+        expect(r.exit_code).to eq(0)
       end
     end
     it 'supports style BitBucket payloads via payload end point' do
       shell('/usr/bin/curl -X POST -d \'{ "push": { "changes": [ { "new": { "name": "production" } } ] } }\' "http://localhost:8088/payload" -q') do |r|
-        expect(r.stdout).to match(%r{^.*success.*$}) # rubocop:disable Rspec/MultipleExpectations
-        expect(r.exit_code).to eq(0) # rubocop:disable Rspec/MultipleExpectations
+        expect(r.stdout).to match(%r{^.*success.*$})
+        expect(r.exit_code).to eq(0)
       end
     end
   end
