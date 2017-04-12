80e0c16fe3a9c8d443dfb5e2b7e4784477894b97
Nikita Akhnin <nike.ahnin@gmail.com>
2017-04-12 22:21:28 +0700

    Add rspec test for FOSS with extra mco arguments

--- spec/classes/webhook/config_spec.rb
+++ spec/classes/webhook/config_spec.rb
@@ -331,6 +331,49 @@ user: "puppet"
           )
         end
       end
+      
+      context 'FOSS with extra mco argument' do
+        let :params do
+          {
+            mco_arguments: '--no-progress'
+          }
+        end
+
+        it do
+          is_expected.to contain_file('webhook.yaml').with(
+            path:   '/etc/webhook.yaml',
+            ensure: 'file',
+            owner:  'root',
+            group:  'root',
+            mode:   '0644',
+            notify: 'Service[webhook]'
+          )
+        end
+        content = '---
+access_logfile: "/var/log/webhook/access.log"
+allow_uppercase: true
+bind_address: "0.0.0.0"
+client_cfg: "/var/lib/peadmin/.mcollective"
+client_timeout: "120"
+command_prefix: "umask 0022;"
+discovery_timeout: "10"
+enable_mutex_lock: false
+enable_ssl: true
+ignore_environments: []
+mco_arguments: "--no-progress"
+pass: "puppet"
+port: "8088"
+prefix: false
+prefix_command: "/bin/echo example"
+protected: true
+r10k_deploy_arguments: "-pv"
+server_software: "WebHook"
+use_mco_ruby: false
+use_mcollective: true
+user: "puppet"
+'
+        it { is_expected.to contain_file('webhook.yaml').with_content(content) }
+      end
     end
   end
 end
