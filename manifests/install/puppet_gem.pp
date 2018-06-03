7441dc0748854715895c411664fb955263779c2f
Andrew N Golovkov <agolovkov@ozon.ru>
2018-06-03 18:28:56 +0300

    remove ::git dependency from r10k::install::puppet_gem

--- manifests/install/puppet_gem.pp
+++ manifests/install/puppet_gem.pp
@@ -1,6 +1,5 @@
 # This class links the r10k binary for Puppet FOSS 4.2 and up
 class r10k::install::puppet_gem {
-  require ::git
 
   if versioncmp("${::puppetversion}", '4.2.0') >= 0 { #lint:ignore:only_variable_string
     file { '/usr/bin/r10k':
