7cd9737f0aec8f1e2e8fd699ed17c47833cadd6a
Kramer, Rudi <rudi.kramer@fnb.co.za>
2018-11-26 13:42:48 +0200

    Removes GCC and make from project

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -26,7 +26,6 @@ describe 'r10k::install', type: :class do
 
         # New versions of this gem do not require these packages
         it { is_expected.not_to contain_class('make') }
-        it { is_expected.not_to contain_package('gcc') }
 
         it { is_expected.to contain_class('r10k::install::gem').with(version: version) }
         it do
