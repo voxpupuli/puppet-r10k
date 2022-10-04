506920858b7f987d624929ae0fd6a07fe4b21b3d
Romain Tartière <romain@blogreen.org>
2022-10-04 08:49:26 -1000

    Fix webhook setup when using older Puppet/Ruby
    Sinatra 3.0 was released and require Ruby 2.6+, but Puppet 6 ships with
    Ruby 2.5.
    
    Adjust the webhook config to install sinatra 2.x if the version of Ruby
    does not support sinatra 3.x.  This should not affect people who already
    installed sinatra, so should not be a breaking change.

--- manifests/params.pp
+++ manifests/params.pp
@@ -155,6 +155,8 @@ class r10k::params {
   $webhook_mco_arguments         = undef
   if $facts['pe_server_version'] == '2016.4.2' {
     $webhook_sinatra_version       = '~> 1.0'
+  } elsif versioncmp(fact('ruby.version'), '2.6.0') < 0 {
+    $webhook_sinatra_version       = '~> 2.0'
   } else {
     $webhook_sinatra_version       = 'installed'
   }
