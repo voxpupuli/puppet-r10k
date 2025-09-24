e20fcb23adb73bb99835eda51720f6998b21143d
Kenyon Ralph <kenyon@kenyonralph.com>
2025-09-24 01:19:55 -0700

    puppet-lint fix: indentation

--- types/webhook/config/chatops.pp
+++ types/webhook/config/chatops.pp
@@ -1,9 +1,11 @@
 # @summary webhook config chatops type
-type R10k::Webhook::Config::Chatops = Struct[{
+type R10k::Webhook::Config::Chatops = Struct[
+  {
     enabled    => Boolean,
     service    => Optional[Enum['slack', 'rocketchat']],
     channel    => Optional[String[1]],
     user       => Optional[String[1]],
     auth_token => Optional[String[1]],
     server_uri => Optional[String[1]],
-}]
+  }
+]
