b6a335ee4b22baee428f2fb2c78d4fcf45c41036
Jo Rhett <geek@jorhett.com>
2026-08-22 13:52:35 -0700

    Allow use of short-term git tokens with r10k
    If given a token path,
      Create a git credential helper to read the token
        The helper will emit stderr message and return quit=true on missing/non-regular or unreadable token files
      Configure the webhook service to utilize the git credential helper

--- manifests/webhook/config.pp
+++ manifests/webhook/config.pp
@@ -3,6 +3,13 @@
 #
 class r10k::webhook::config (
 ) {
+  file { '/etc/voxpupuli':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
   file { 'webhook.yml':
     ensure  => $r10k::webhook::config_ensure,
     path    => $r10k::webhook::config_path,
@@ -10,5 +17,29 @@ class r10k::webhook::config (
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
+  }
+
+  if $r10k::webhook::github_token_path {
+    file { '/etc/voxpupuli/r10k-git-credential-helper':
+      ensure  => file,
+      owner   => 'root',
+      group   => $r10k::webhook::service_user,
+      mode    => '0550',
+      content => epp('r10k/r10k-git-credential-helper.epp', {
+        'token_path' => shellquote($r10k::webhook::github_token_path),
+      }),
+    }
+
+    file { '/etc/voxpupuli/r10k.gitconfig':
+      ensure  => file,
+      owner   => 'root',
+      group   => $r10k::webhook::service_user,
+      mode    => '0440',
+      content => "[credential \"https://github.com\"]\n  helper = /etc/voxpupuli/r10k-git-credential-helper\n",
+      require => [
+        File['/etc/voxpupuli'],
+        File['/etc/voxpupuli/r10k-git-credential-helper'],
+      ],
+    }
   }
 }
--- manifests/webhook/service.pp
+++ manifests/webhook/service.pp
@@ -6,10 +6,15 @@ class r10k::webhook::service () {
     ensure => $r10k::webhook::service_ensure,
     enable => $r10k::webhook::service_enabled,
   }
-  if $r10k::webhook::service_user {
-    systemd::dropin_file { 'user.conf':
-      unit    => 'webhook-go.service',
-      content => "[Service]\nUser=${r10k::webhook::service_user}\n",
-    }
+
+  systemd::dropin_file { 'webhook-go.service-override':
+    unit     => 'webhook-go.service',
+    filename => 'override.conf',
+    content  => epp('r10k/webhook-service-override.epp', {
+      'service_user'      => $r10k::webhook::service_user,
+      'github_token_path' => $r10k::webhook::github_token_path,
+    }),
+    require  => Class['r10k::webhook::config'],
+    notify   => Service['webhook-go.service'],
   }
 }
